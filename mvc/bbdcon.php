9862e67ce695218bc5dfdd87bc256b666955bed9
Dario <dcesteraguirre@gmail.com>
2022-11-07 15:33:35 +0000

    sis

---
+++ mvc/bbdcon.php
@@ -0,0 +1,7 @@
+require "../bbdcon.php";
+
+    /recurso/metodo/parametro1/parametro2
+
+    /recurso -> controlador
+    /metodos -> metodos o funciones dentro del controladores
+    /parametros : opcion1 
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+    echo"<h2>Contenido Público<h2>";
+    require "../start.php";
+
+?>

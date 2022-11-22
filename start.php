48c626cf758978e14a55c79d7d280c34c5501963
Dario <dcesteraguirre@gmail.com>
2022-11-22 11:36:51 +0000

    s

--- start.php
+++ start.php
@@ -1,2 +1,23 @@
 <?php
 echo 'Contenido Privado<br>';
+     $dsn = "mysql:dbname=agenda;host=db";
+     $usuario = "dbuser";
+     $clave = "secret";
+     
+     try {
+        $bd = new PDO($dsn,$usuario,$clave);
+        $sql = "select usuario,password from credenciales";
+        $registros = $bd->query($sql);
+        echo "<br>Numero de registros devueltos: " . $registros->rowCount();
+        if($registros->rowCount() > 0){
+            foreach($registros as $fila){
+                echo "<br>Usuario :  " .$fila["usuario"];
+                echo "<br>Contraseña :  " .$fila["password"];
+            }
+        }else{
+            echo "<br>No se ha devuelto ninguna fila";
+        }
+
+    } catch (PDOException $e){
+        echo "<br>Mensaje de la excepcion : " . $e->getMessage();
+    }
